9cc100465fa97ccaa00a8221ae017792ec580381
unacyanman <166233150+unacyanman@users.noreply.github.com>
2024-05-30 07:04:30 +0000

    [Update] ルーティングの記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "homes#top"
+  devise_for :users
   
-  resources :users, only: [:show, :edit]
+  resources :users, only: [:new, :show, :edit, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
