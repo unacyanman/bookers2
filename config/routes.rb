36701ebd4ca12006444e5574dc4776c4d7e47498
unacyanman <166233150+unacyanman@users.noreply.github.com>
2024-06-03 14:05:35 +0000

    [Fix] ログアウト後

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
-  root to: "homes#top"
+  root "homes#top"
+  get '/homes/top', to: 'homes#top', as: 'homes_path'
+  
   get 'books/new'
   post 'books' => 'books#create'
   
   get '/about', to: 'homes#about', as: 'about'
   get '/books', to: 'books#index', as: 'books_index'
   get '/top', to: 'homes#top', as: 'top'
+  
   devise_for :users
+  
   get 'users/show'
   get 'users/edit'
+  get '/users', to: 'users#index', as: 'users_index'
   
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
