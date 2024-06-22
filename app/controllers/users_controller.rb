d0cdc9b28a826425d468560fb3cc40b3a8d90a50
unacyanman <166233150+unacyanman@users.noreply.github.com>
2024-06-22 13:16:52 +0000

    [Fix]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to '/users/index'
+      redirect_to user_path(@user.id)
     else
       render 'edit'
     end
