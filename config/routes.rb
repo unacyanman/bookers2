16e95d8557912296f5d8be29103e7a71d5b911bf
unacyanman <166233150+unacyanman@users.noreply.github.com>
2024-06-23 13:14:17 +0000

    [Fix]

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   
   devise_for :users
   
-  resources :users, onlu: [:new, :create, :index, :show]
-  resources :books, onlu: [:new, :create, :index, :show, :edit, :update,]
+  resources :users, only: [:new, :create, :index, :show, :edit]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
